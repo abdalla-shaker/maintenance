f2bb48e84bfa9c659308c7ce4696e87cef5d38c6
abdalla-shaker <abdallashaker143@gmail.com>
2024-08-17 16:21:19 +0300

    adding a section

--- assets/app/app.js
+++ assets/app/app.js
@@ -1,29 +1,50 @@
 const burgerIcon = document.getElementById("burger-icon");
 const header = document.getElementById("header");
+const overLay = header.querySelector(".nav-overLay");
+const companiesContainer = document.getElementById("companies-container");
+
+class Product {
+  constructor(src) {
+    this.imgSrc = src;
+  }
+}
+
+class SetProduct {
+  settingProductsArray() {
+    let productsArray = [];
+    for (let num = 1; num <= 15; num++) {
+      productsArray.push(new Product(`company logo ${num}`));
+    }
+    return productsArray;
+  }
+
+  render() {
+    const prodArrays = this.settingProductsArray();
+    for (const prod of prodArrays) {
+      const imgCont = document.createElement("div");
+      const prodSrcWithoutSpaces = prod.imgSrc.replace(/\s/g, "");
+      imgCont.className = prodSrcWithoutSpaces;
+      imgCont.innerHTML = `
+      <a href="${prod.imgSrc}.html">
+        <img src="assets/images/landing page/${prod.imgSrc}.webp" alt="logo image" class="${prodSrcWithoutSpaces}"/>
+      </a>
+      `;
+      companiesContainer.append(imgCont);
+    }
+  }
+}
+
+const app = new SetProduct();
+app.render();
 
 const togglingActiveClass = function (element) {
   element.classList.toggle("active");
 };
 
-const creatingOverLay = function () {
-  const overLay = document.createElement("div");
-  overLay.className = "nav-overLay";
-  return overLay;
-};
-
 burgerIcon.addEventListener("click", () => {
   togglingActiveClass(header);
+});
 
-  let existingOverLay = header.querySelector(".nav-overLay");
-
-  if (header.classList.contains("active")) {
-    if (!existingOverLay) {
-      const overLay = creatingOverLay();
-      header.appendChild(overLay);
-    }
-  } else {
-    if (existingOverLay) {
-      existingOverLay.remove();
-    }
-  }
+overLay.addEventListener("click", () => {
+  togglingActiveClass(header);
 });
